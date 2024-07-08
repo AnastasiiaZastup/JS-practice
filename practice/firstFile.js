2c138429c13fa34f92229f0d3abedeced903a724
Anastasia18432 <zastupanastasia24@gmail.com>
2024-07-08 17:31:22 +0300

    add

---
+++ practice/firstFile.js
@@ -0,0 +1,25 @@
+//-----------------------------------------------------------
+//work with date and time
+
+//const now = new Date("2024-07-08");
+//new Date.parse("2024-07-08");
+
+//console.log(now.setHours(12));
+//console.log(now);
+
+//console.log(now.getFullYear());
+//console.log(now.getDate());
+//console.log(now.getDay());
+//console.log(now.getHours());
+//console.log(now.getTime());
+
+//let start = new Date();
+
+//for (let i = 0; i < 100; i++) {
+//  let some = i + 1;
+//  console.log(`${some}`);
+//}
+
+//let end = new Date();
+
+//console.log(`What we had: ${end - start}`);

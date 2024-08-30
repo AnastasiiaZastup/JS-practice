e383e2f5eff4ac093f9a40d2d2ef6c74f7c516dc
Anastasia18432 <zastupanastasia7@gmail.com>
2024-08-30 12:47:34 +0300

    add webpack

---
+++ webpack-demo/src/index.js
@@ -0,0 +1,21 @@
+// http://localhost:8888/webpack-demo/src/
+
+// json-server --watch db.json --port 3001
+
+import tabs from "./js/modules/tabs";
+import modal from "./js/modules/modal";
+import timer from "./js/modules/timer";
+import calc from "./js/modules/calc";
+import cards from "./js/modules/cards";
+import forms from "./js/modules/forms";
+import slider from "./js/modules/slider";
+
+window.addEventListener("DOMContentLoaded", function () {
+  tabs();
+  modal();
+  calc();
+  cards();
+  forms();
+  slider();
+  timer();
+});
---
+++ webpack-demo/src/js/modules/index.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const tabs = require("./modules/tabs");
+  const modal = require("./modules/modal");
+  const timer = require("./modules/timer");
+  const calc = require("./modules/calc");
+  const cards = require("./modules/cards");
+  const forms = require("./modules/forms");
+  const slider = require("./modules/slider");
+
+  tabs();
+  modal();
+  timer();
+  calc();
+  cards();
+  forms();
+  slider();
+});
